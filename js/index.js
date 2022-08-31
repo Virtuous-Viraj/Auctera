40b5f3f587a77e6efd9b9a1d64c19307598082e6
Viraj Nerlekar <90627072+Virtuous-Viraj@users.noreply.github.com>
2022-08-31 11:53:40 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -21,14 +21,7 @@ $(document).ready(function(){
         })
     }
 
-    //Scroll To Top
-    $(window).scroll(function () {
-        if ($(this).scrollTop() >= 500) {
-            $(".back-to-top").fadeIn(200);
-        } else {
-            $(".back-to-top").fadeOut(200);
-        } 
-    })
+
 
     //Search Toggle Button
     $(function(){
@@ -37,21 +30,6 @@ $(document).ready(function(){
         })
     })
 
-    //Menu Toggle Button
-    $('.menu-toggle').click(function(){
-        $('.menu-toggle').toggleClass('active');
-    })
-
-    //Filter Section
-    $(".gallery-button").click(function () {
-        var name = $(this).attr('data-filter');
-        if (name == "all") {
-            $(".shot-thumbnail").show('2000');
-        } else {
-            $(".shot-thumbnail").not("." + name).hide('2000');
-            $(".shot-thumbnail").filter("." + name).show('2000');
-        }
-    })
 
     $('.navigation a').click(function(){
         $(this).addClass("active").siblings().removeClass('active');
@@ -59,26 +37,6 @@ $(document).ready(function(){
 
 
 
-    //owl carousel
-    $('#our-team').owlCarousel({
-        loop:true,
-        autoplay:true,
-        margin:10,
-        responsiveClass:true,
-        nav:false,
-        dots:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:3
-            }
-        }
-    }) 
 
 
  })
